103e096cfc0ecce0e1ebe82c711630c67781cd1d
Anri20 <88647934+Anri20@users.noreply.github.com>
2021-10-25 22:59:10 +0700

    Update Pegawai
    tak tambahi data member, constructors, properties ama region-nya, perlu buat cabang

--- OnlineMart_Trivial/OnlineMart_LIB/Pegawai.cs
+++ OnlineMart_Trivial/OnlineMart_LIB/Pegawai.cs
@@ -10,5 +10,32 @@ namespace OnlineMart_LIB
 {
     public class Pegawai
     {
+        private int idPegawai;
+        private string nama;
+        private string email;
+        private string password;
+        private string telepon;
+
+        #region Constructors
+        public Pegawai(int idPegawai, string nama, string email, string password, string telepon)
+        {
+            IdPegawai = idPegawai;
+            Nama = nama;
+            Email = email;
+            Password = password;
+            Telepon = telepon;
+        }
+        #endregion
+
+        #region Properties
+        public int IdPegawai { get => idPegawai; set => idPegawai = value; }
+        public string Nama { get => nama; set => nama = value; }
+        public string Email { get => email; set => email = value; }
+        public string Password { get => password; set => password = value; }
+        public string Telepon { get => telepon; set => telepon = value; }
+        #endregion
+
+        #region Methods
+        #endregion
     }
 }
