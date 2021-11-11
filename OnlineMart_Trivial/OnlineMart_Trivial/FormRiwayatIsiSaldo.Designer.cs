b4fc79580abe8aca03c7821d9259341a86f0ae1a
Brianowen926 <brianowen926@gmail.com>
2021-11-11 09:45:35 +0700

    riwayat_isi_saldos done

--- OnlineMart_Trivial/OnlineMart_Trivial/FormRiwayatIsiSaldo.Designer.cs
+++ OnlineMart_Trivial/OnlineMart_Trivial/FormRiwayatIsiSaldo.Designer.cs
@@ -29,33 +29,125 @@
         private void InitializeComponent()
         {
             this.dataGridViewRiwayat = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonTambah = new System.Windows.Forms.Button();
+            this.comboBoxKriteria = new System.Windows.Forms.ComboBox();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.textBoxKriteria = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRiwayat)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridViewRiwayat
             // 
             this.dataGridViewRiwayat.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewRiwayat.Location = new System.Drawing.Point(54, 50);
+            this.dataGridViewRiwayat.Location = new System.Drawing.Point(70, 120);
             this.dataGridViewRiwayat.Name = "dataGridViewRiwayat";
             this.dataGridViewRiwayat.Size = new System.Drawing.Size(478, 244);
             this.dataGridViewRiwayat.TabIndex = 0;
             this.dataGridViewRiwayat.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewRiwayat_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Cari Berdasarkan :";
+            // 
+            // buttonTambah
+            // 
+            this.buttonTambah.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonTambah.AutoSize = true;
+            this.buttonTambah.BackColor = System.Drawing.Color.Transparent;
+            this.buttonTambah.BackgroundImage = global::OnlineMart_Trivial.Properties.Resources.Button_Leave;
+            this.buttonTambah.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonTambah.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonTambah.FlatAppearance.BorderSize = 0;
+            this.buttonTambah.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonTambah.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Bold);
+            this.buttonTambah.ForeColor = System.Drawing.Color.White;
+            this.buttonTambah.Location = new System.Drawing.Point(17, 399);
+            this.buttonTambah.Name = "buttonTambah";
+            this.buttonTambah.Size = new System.Drawing.Size(142, 39);
+            this.buttonTambah.TabIndex = 7;
+            this.buttonTambah.Text = "Tambah Data";
+            this.buttonTambah.UseVisualStyleBackColor = false;
+            this.buttonTambah.Click += new System.EventHandler(this.buttonTambah_Click);
+            // 
+            // comboBoxKriteria
+            // 
+            this.comboBoxKriteria.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxKriteria.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxKriteria.FormattingEnabled = true;
+            this.comboBoxKriteria.Items.AddRange(new object[] {
+            "Id",
+            "Nama Cabang",
+            "Alamat",
+            "Pegawai"});
+            this.comboBoxKriteria.Location = new System.Drawing.Point(27, 54);
+            this.comboBoxKriteria.Name = "comboBoxKriteria";
+            this.comboBoxKriteria.Size = new System.Drawing.Size(248, 28);
+            this.comboBoxKriteria.TabIndex = 10;
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClose.AutoSize = true;
+            this.buttonClose.BackColor = System.Drawing.Color.Transparent;
+            this.buttonClose.BackgroundImage = global::OnlineMart_Trivial.Properties.Resources.Button_Leave;
+            this.buttonClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonClose.FlatAppearance.BorderSize = 0;
+            this.buttonClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Bold);
+            this.buttonClose.ForeColor = System.Drawing.Color.White;
+            this.buttonClose.Location = new System.Drawing.Point(629, 399);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(128, 39);
+            this.buttonClose.TabIndex = 8;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = false;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // textBoxKriteria
+            // 
+            this.textBoxKriteria.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxKriteria.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxKriteria.Location = new System.Drawing.Point(321, 55);
+            this.textBoxKriteria.Name = "textBoxKriteria";
+            this.textBoxKriteria.Size = new System.Drawing.Size(446, 26);
+            this.textBoxKriteria.TabIndex = 6;
+            this.textBoxKriteria.TextChanged += new System.EventHandler(this.textBoxKriteria_TextChanged);
             // 
             // FormRiwayatIsiSaldo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonTambah);
+            this.Controls.Add(this.comboBoxKriteria);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.textBoxKriteria);
             this.Controls.Add(this.dataGridViewRiwayat);
             this.Name = "FormRiwayatIsiSaldo";
             this.Text = "FormRiwayatIsiSaldo";
+            this.Load += new System.EventHandler(this.FormRiwayatIsiSaldo_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRiwayat)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridViewRiwayat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonTambah;
+        private System.Windows.Forms.ComboBox comboBoxKriteria;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.TextBox textBoxKriteria;
     }
 }
--- OnlineMart_Trivial/OnlineMart_Trivial/FormRiwayatIsiSaldo.cs
+++ OnlineMart_Trivial/OnlineMart_Trivial/FormRiwayatIsiSaldo.cs
@@ -19,7 +19,9 @@ namespace OnlineMart_Trivial
         }
         public List<Riwayat_isi_saldo> listRiwayat = new List<Riwayat_isi_saldo>();
 
-        private void dataGridViewRiwayat_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        #region Methods
+
+        private void FormatDataGrid()
         {
             //Kosongi semua kolom di datagridview
             dataGridViewRiwayat.Columns.Clear();
@@ -40,5 +42,169 @@ namespace OnlineMart_Trivial
             dataGridViewRiwayat.AllowUserToAddRows = false;
             dataGridViewRiwayat.ReadOnly = true;
         }
+        private void TampilDataGrid()
+        {
+            //Kosongi isi datagridview
+            dataGridViewRiwayat.Rows.Clear();
+
+            if (listRiwayat.Count > 0)
+            {
+                foreach (Riwayat_isi_saldo r in listRiwayat)
+                {
+                    dataGridViewRiwayat.Rows.Add(r.Id, r.Waktu, r.IsiSaldo, r.Pelanggan.Id);
+                }
+            }
+            else
+            {
+                dataGridViewRiwayat.DataSource = null;
+            }
+
+            //Tampilkan button Ubah dan Hapus
+            if (!dataGridViewRiwayat.Columns.Contains("btnUbahGrid"))
+            {
+                DataGridViewButtonColumn bcolUbah = new DataGridViewButtonColumn();
+
+                bcolUbah.HeaderText = "Aksi";
+                bcolUbah.Text = "Ubah";
+                bcolUbah.Name = "btnUbahGrid";
+                bcolUbah.UseColumnTextForButtonValue = true;
+                bcolUbah.FlatStyle = FlatStyle.Flat;
+                dataGridViewRiwayat.Columns.Add(bcolUbah);
+
+                DataGridViewButtonColumn bcolHapus = new DataGridViewButtonColumn();
+
+                bcolHapus.HeaderText = "Aksi";
+                bcolHapus.Text = "Hapus";
+                bcolHapus.Name = "btnHapusGrid";
+                bcolHapus.UseColumnTextForButtonValue = true;
+                bcolHapus.FlatStyle = FlatStyle.Flat;
+
+                dataGridViewRiwayat.Columns.Add(bcolHapus);
+
+                //Button tambah ke keranjang
+                DataGridViewButtonColumn bcolTambahKeranjang = new DataGridViewButtonColumn();
+
+                bcolTambahKeranjang.HeaderText = "Aksi";
+                bcolTambahKeranjang.Text = "Tambahkan ke keranjang";
+                bcolTambahKeranjang.Name = "btnTambahKeranjang";
+                bcolTambahKeranjang.UseColumnTextForButtonValue = true;
+                bcolTambahKeranjang.FlatStyle = FlatStyle.Flat;
+                bcolTambahKeranjang.DefaultCellStyle.Font = new Font("Montserrat", 9, FontStyle.Bold);
+                bcolTambahKeranjang.DefaultCellStyle.ForeColor = Color.White;
+                bcolTambahKeranjang.DefaultCellStyle.BackColor = Color.FromArgb(227, 65, 35);
+
+                dataGridViewRiwayat.Columns.Add(bcolTambahKeranjang);
+            }
+            #endregion
+        }
+
+        private void FormRiwayatIsiSaldo_Load(object sender, EventArgs e)
+        {
+            // Panggil Method untuk menambah kolom pada datagridview
+            FormatDataGrid();
+
+            // Tampilkan semua data
+            listRiwayat = Riwayat_isi_saldo.BacaData("", "");
+
+            //Tampilkan semua isi list di datagridview (Panggil method TampilDataGridView)
+            TampilDataGrid();
+
+            comboBoxKriteria.Text = "Id";
+        }
+
+        #region Textbox
+        private void textBoxKriteria_TextChanged(object sender, EventArgs e)
+        {
+            string kriteria = "";
+            switch (comboBoxKriteria.Text)
+            {
+                case "Id":
+                    kriteria = "R.id";
+                    break;
+
+                case "Waktu":
+                    kriteria = "R.waktu";
+                    break;
+
+                case "Isi Saldo":
+                    kriteria = "R.isiSaldo";
+                    break;
+
+                case "Pelanggan":
+                    kriteria = "P.nama";
+                    break;
+            }
+
+            listRiwayat = Riwayat_isi_saldo.BacaData(kriteria, textBoxKriteria.Text);
+            FormatDataGrid();
+            TampilDataGrid();
+            #endregion
+        }
+
+        #region DataGrid
+        private void dataGridViewRiwayat_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            try
+            {
+                //Menghapus data bila button hapus diklik
+                int id = int.Parse(dataGridViewRiwayat.CurrentRow.Cells["Id"].Value.ToString());
+
+
+                //Kalau button hapus diklik
+                if (e.ColumnIndex == dataGridViewRiwayat.Columns["btnHapusGrid"].Index && e.RowIndex >= 0)
+                {
+                    string idHapus = dataGridViewRiwayat.CurrentRow.Cells["Id"].Value.ToString();
+                    string namaHapus = dataGridViewRiwayat.CurrentRow.Cells["Nama"].Value.ToString();
+
+                    //User ditanya sesuai dibawah
+                    DialogResult hasil = MessageBox.Show(this, "Anda yakin akan menghapus Id " + idHapus + " - " + namaHapus + "?",
+                                                         "HAPUS", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    //Kalau User klik yes barang akan dihapus
+                    if (hasil == DialogResult.Yes)
+                    {
+                        Boolean hapus = Cabang.HapusData(id);
+
+                        if (hapus == true)
+                        {
+                            MessageBox.Show("Penghapusan data berhasil");
+                            // Refresh Halaman
+                           FormRiwayatIsiSaldo_Load(sender, e);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Penghapusan data gagal");
+                        }
+                    }
+
+                }
+                //Kalau button ubah diklik
+                if (e.ColumnIndex == dataGridViewRiwayat.Columns["btnUbahGrid"].Index && e.RowIndex >= 0)
+                {
+                    FormUbahCabang.IdDipilih = id;
+                    FormUbahCabang frm = new FormUbahCabang();
+                    frm.Owner = this;
+                    frm.Show();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Terjadi Error. Pesan kesalahan : " + ex.Message, "Kesalahan");
+            }
+        }
+        #endregion
+
+        #region Button
+        private void buttonTambah_Click(object sender, EventArgs e)
+        {
+            FormRiwayatIsiSaldo tambah = new FormRiwayatIsiSaldo();
+            tambah.Owner = this;
+            tambah.Show();
+        }
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        #endregion
     }
 }
